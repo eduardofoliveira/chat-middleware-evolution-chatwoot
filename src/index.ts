0956dc6f22d7db831e23bb9231175fae111abf27
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-09-16 10:04:36 -0300

    Change port env

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,8 @@ server.register(evolutionRoutes);
 
 const start = async () => {
   try {
-    await server.listen({ port: 3000, host: "0.0.0.0" });
-    console.log("🚀 Servidor rodando em http://localhost:3000");
+    await server.listen({ port: Number(process.env.PORT) || 3001, host: "0.0.0.0" });
+    console.log(`🚀 Servidor rodando em http://localhost:${process.env.PORT || 3001}`);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
