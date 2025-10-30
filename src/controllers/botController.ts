3f1995cf2787c394975d28f85467c3bc61a21de0
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-10-30 16:09:08 -0300

    Add Bot Routes

--- src/controllers/botController.ts
+++ src/controllers/botController.ts
@@ -8,6 +8,39 @@ const index = async (request: FastifyRequest, reply: FastifyReply) => {
 		bot_name: string;
 	};
 	const body = request.body;
+	const { content_type, content, inbox, message_type, sender, event } =
+		body as {
+			content_type: string;
+			content: string;
+			inbox: {
+				id: number;
+				name: string;
+			};
+			message_type: string;
+			sender: {
+				id: number;
+				name: string;
+			};
+			event: string;
+		};
+	const { id: inboxId, name: inboxName } = inbox;
+
+	if (
+		event === "message_created" &&
+		message_type === "incoming" &&
+		inboxName === "1135880866" &&
+		inboxId === 49
+	) {
+		if (content_type === "text" && content) {
+			console.log("Mensagem de texto recebida no bot Cosmos:");
+			console.log(`Remetente: ${sender.name} (ID: ${sender.id})`);
+			console.log(`Conteúdo: ${content}`);
+
+			return reply.send({
+				message: `Bot flow endpoint hit for account ${account_id} and bot ${bot_name}`,
+			});
+		}
+	}
 
 	console.log(
 		JSON.stringify(
