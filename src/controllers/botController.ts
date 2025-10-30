136fff8e4c35d2a0de4985b353e386d7d695b831
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-10-30 16:14:14 -0300

    Add Bot Routes

--- src/controllers/botController.ts
+++ src/controllers/botController.ts
@@ -8,21 +8,35 @@ const index = async (request: FastifyRequest, reply: FastifyReply) => {
 		bot_name: string;
 	};
 	const body = request.body;
-	const { content_type, content, inbox, message_type, sender, event } =
-		body as {
-			content_type: string;
-			content: string;
-			inbox: {
-				id: number;
-				name: string;
-			};
-			message_type: string;
-			sender: {
-				id: number;
-				name: string;
-			};
-			event: string;
+	const {
+		content_type,
+		content,
+		inbox,
+		message_type,
+		sender,
+		event,
+		conversation,
+	} = body as {
+		content_type: string;
+		content: string;
+		inbox: {
+			id: number;
+			name: string;
 		};
+		message_type: string;
+		sender: {
+			id: number;
+			name: string;
+		};
+		event: string;
+		conversation?: {
+			messages?: Array<{
+				attachments?: Array<{
+					data_url: string;
+				}>;
+			}>;
+		};
+	};
 	const { id: inboxId, name: inboxName } = inbox;
 
 	if (
@@ -35,6 +49,20 @@ const index = async (request: FastifyRequest, reply: FastifyReply) => {
 			console.log("Mensagem de texto recebida no bot Cosmos:");
 			console.log(`Remetente: ${sender.name} (ID: ${sender.id})`);
 			console.log(`Conteúdo: ${content}`);
+
+			return reply.send({
+				message: `Bot flow endpoint hit for account ${account_id} and bot ${bot_name}`,
+			});
+		}
+
+		if (
+			content_type === "text" &&
+			!content &&
+			conversation?.messages?.[0]?.attachments?.[0]?.data_url
+		) {
+			console.log("Mensagem de anexo recebida no bot Cosmos:");
+			console.log(`Remetente: ${sender.name} (ID: ${sender.id})`);
+			console.log(`Anexo: ${conversation.messages[0].attachments[0].data_url}`);
 
 			return reply.send({
 				message: `Bot flow endpoint hit for account ${account_id} and bot ${bot_name}`,
