d6ef06f1664fc6ed309f7c190b6ac126b7f21c4a
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-10-30 15:57:40 -0300

    Add Bot Routes

--- src/controllers/botController.ts
+++ src/controllers/botController.ts
@@ -1,5 +1,7 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 
+const TokenBotDataCosmos = "2h9w9JKmSRHL9E9433fLscN6";
+
 const index = async (request: FastifyRequest, reply: FastifyReply) => {
 	const { account_id, bot_name } = request.params as {
 		account_id: number;
@@ -7,11 +9,17 @@ const index = async (request: FastifyRequest, reply: FastifyReply) => {
 	};
 	const body = request.body;
 
-	console.log({
-		account_id,
-		bot_name,
-		body,
-	});
+	console.log(
+		JSON.stringify(
+			{
+				account_id,
+				bot_name,
+				body,
+			},
+			null,
+			2,
+		),
+	);
 
 	return reply.send({
 		message: `Bot flow endpoint hit for account ${account_id} and bot ${bot_name}`,
