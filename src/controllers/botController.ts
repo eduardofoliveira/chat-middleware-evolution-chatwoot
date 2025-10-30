ab5c9931685459ea5d97fa5f95d8fc4ed6a2842b
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-10-30 16:39:42 -0300

    Add Bot Routes

--- src/controllers/botController.ts
+++ src/controllers/botController.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import type { FastifyReply, FastifyRequest } from "fastify";
 
 const TokenBotDataCosmos = "2h9w9JKmSRHL9E9433fLscN6";
@@ -50,6 +51,43 @@ const index = async (request: FastifyRequest, reply: FastifyReply) => {
 			console.log(`Remetente: ${sender.name} (ID: ${sender.id})`);
 			console.log(`Conteúdo: ${content}`);
 
+			await axios.post(
+				"https://cloudcom-team.atlassian.net/rest/api/3/issue/10000/comment",
+				{
+					body: {
+						version: 1,
+						type: "doc",
+						content: [
+							{
+								type: "paragraph",
+								content: [
+									{ type: "text", text: "Nome: ", marks: [{ type: "strong" }] },
+									{ type: "text", text: `${sender.name}\r\n` },
+									{
+										type: "text",
+										text: "Numero: ",
+										marks: [{ type: "strong" }],
+									},
+									{ type: "text", text: `${sender.id}\r\n\r\n` },
+									{
+										type: "text",
+										text: "Mensagem: \r\n",
+										marks: [{ type: "strong" }],
+									},
+									{ type: "text", text: content },
+								],
+							},
+						],
+					},
+				},
+				{
+					headers: {
+						Authorization:
+							"Basic ZWR1YXJkb0BjbG91ZGNvbS5jb20uYnI6QVRBVFQzeEZmR0YwempYSFpMc2l6QlJZYlFCbXoyVF9SREYwNmtfZno2Q3N1T0E2eFpBQllEaC1RYXZzaUlDMk9rbjVHangxZGpZSHJlUUJPUExqUXZ4cUtZMm0yb0U3OUlZLW1zVXZkZ0lfY293TTE0VXZybGJJa0s2bmZnUDA3SnY5NWIyTDVBTEFTWTlPZVhmeVJRdk83Z0hwT1NBaVQtTlpPYmxFV0RScF9TZEoxeGhfVWlnPTg1NUQ5REVC",
+					},
+				},
+			);
+
 			return reply.send({
 				message: `Bot flow endpoint hit for account ${account_id} and bot ${bot_name}`,
 			});
@@ -63,6 +101,46 @@ const index = async (request: FastifyRequest, reply: FastifyReply) => {
 			console.log("Mensagem de anexo recebida no bot Cosmos:");
 			console.log(`Remetente: ${sender.name} (ID: ${sender.id})`);
 			console.log(`Anexo: ${conversation.messages[0].attachments[0].data_url}`);
+
+			await axios.post(
+				"https://cloudcom-team.atlassian.net/rest/api/3/issue/10000/comment",
+				{
+					body: {
+						version: 1,
+						type: "doc",
+						content: [
+							{
+								type: "paragraph",
+								content: [
+									{ type: "text", text: "Nome: ", marks: [{ type: "strong" }] },
+									{ type: "text", text: `${sender.name}\r\n` },
+									{
+										type: "text",
+										text: "Numero: ",
+										marks: [{ type: "strong" }],
+									},
+									{ type: "text", text: `${sender.id}\r\n\r\n` },
+									{
+										type: "text",
+										text: "Mensagem: \r\n",
+										marks: [{ type: "strong" }],
+									},
+									{
+										type: "text",
+										text: conversation.messages[0].attachments[0].data_url,
+									},
+								],
+							},
+						],
+					},
+				},
+				{
+					headers: {
+						Authorization:
+							"Basic ZWR1YXJkb0BjbG91ZGNvbS5jb20uYnI6QVRBVFQzeEZmR0YwempYSFpMc2l6QlJZYlFCbXoyVF9SREYwNmtfZno2Q3N1T0E2eFpBQllEaC1RYXZzaUlDMk9rbjVHangxZGpZSHJlUUJPUExqUXZ4cUtZMm0yb0U3OUlZLW1zVXZkZ0lfY293TTE0VXZybGJJa0s2bmZnUDA3SnY5NWIyTDVBTEFTWTlPZVhmeVJRdk83Z0hwT1NBaVQtTlpPYmxFV0RScF9TZEoxeGhfVWlnPTg1NUQ5REVC",
+					},
+				},
+			);
 
 			return reply.send({
 				message: `Bot flow endpoint hit for account ${account_id} and bot ${bot_name}`,
